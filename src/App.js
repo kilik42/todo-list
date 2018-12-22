dfb45442676b0e34ab7255eaf78294b609d4774d
marvin evins <maevins@cps.edu>
2018-12-22 04:08:04 -0600

    added state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
-import React, { Component } from 'react';
-import TodoInput from './components/TodoInput';
-import TodoList from './components/TodoList';
-
+import React, { Component } from "react";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
 import "bootstrap/dist/css/bootstrap.min.css";
- import uuid from "uuid";
+import uuid from "uuid";
 
 class App extends Component {
+  state = {
+    items: [],
+    id: 0,
+    item: "",
+    editItem: false
+  };
+  handleChange = e => {
+    this.setState({
+      item: e.target.value
+    });
+  };
+  handleSubmit=(e)=>{
+      e.preventDefault();
+
+      const newItem= {
+        id.this.state.id,
+        item.this.state.item
+      }
+      const updatedItems = [...this.state.items, newItem]
+      this.setState({
+        items:updatedItems,
+        
+      })
+
+
+  }
   render() {
     return (
-      
-
-
-
-
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-4">
-         <h3 className="text-capitalize text-center">
-           todo input
-         </h3>
-          <TodoInput/>
-          <TodoList/>
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleChange}
+            />
+            <TodoList />
           </div>
         </div>
       </div>
-
     );
   }
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,7 +6,17 @@ import React, { Component } from 'react';
 export class TodoItem extends Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div>Hello from the item</div>
+     <li className="list-group-item text-capitalized-flex justify-content-between my-2">
+       <h6>title</h6>
+       <div className="to-icon">
+         <span className="mx-2 text-success">
+            <i className="fas fa-pen"></i>
+         </span>
+         <span className="mx-2 text-danger">
+            <i className="fas fa-trash"></i> 
+         </span>
+       </div>
+     </li>
     );
   }
 }
